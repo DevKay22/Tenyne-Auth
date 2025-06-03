f30ff4712e41e233487ddaf0652a9dedd8d0d094
Kayode <drkay4sure@gmail.com>
2025-06-03 23:34:02 +0100

    controller updated

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,26 +1,50 @@
-
-
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {v4:uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const User = require('../model/User');
 
 exports.register = async (req, res) => {
     const { name, email, password } = req.body;
+
     try {
-        let user = await User.findOne({email});
-        if(user) { return res.status(400).json({error:false, message:"User already exists"})}
-        else{
-            const securepassword= await bcrypt.hash(password,10);
-            user = new User({id:uuidv4(), name:name,email:email,password:securepassword});
-            await user.save();
-            const token = jwt.sign({id:user.id},process.env.JWT_SECRET,{expireIn: process.env.JWT_EXPIRES_IN})
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ error: true, message: "User already exists" });
         }
 
-        await res.status(201).json
-    } catch (error) {
-        res.status(500).json({error:true, message:"server error", data:error.tostring()})
-    }
+        const securePassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            id: uuidv4(),
+            name,
+            email,
+            password: securePassword
+        });
 
-    res.send({name: name, email: email, password: password})
-}
+        await newUser.save();
+
+        const token = jwt.sign(
+            { id: newUser.id },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+        );
+
+        return res.status(201).json({
+            error: false,
+            message: "User registered successfully",
+            token,
+            user: {
+                id: newUser.id,
+                name: newUser.name,
+                email: newUser.email
+            }
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Server error",
+            data: error.toString()
+        });
+    }
+};
